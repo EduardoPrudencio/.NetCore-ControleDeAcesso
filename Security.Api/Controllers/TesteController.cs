8c2e325b0f45fe5ec6763568e90e61c126d7c433
EduardoPrudencio <eduardolobo.esp@gmail.com>
2019-09-01 22:16:22 -0300

    Criando controller para testar o JWT

---
+++ Security.Api/Controllers/TesteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Security.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+        public ActionResult Get()
+        {
+            return Ok("Muito bom!!!");
+        }
+    }
+}
